4fa5b1a2e11e03036460eb86dc742c2d082f0777
David Sttivend <dsangel@gmail.com>
2019-12-03 17:11:21 -0500

    Added some changes to typeDefs and models

--- workshop/server/src/models.js
+++ workshop/server/src/models.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const petSchema = new mongoose.Schema({
   name: String,
   age: Number,
+  race: String,
 });
 
 const Pet = mongoose.model('Pet', petSchema);
--- workshop/server/src/typeDefs.js
+++ workshop/server/src/typeDefs.js
@@ -2,15 +2,6 @@ const {gql} = require('apollo-server');
 
 // TODO: delete this
 
-const typeDefs = gql`
-  type Query {
-    pets: [Pet!]!
-  }
-
-  type Pet {
-    name: String
-    age: Int
-  }
-`;
+const typeDefs = gql``;
 
 module.exports = typeDefs;
