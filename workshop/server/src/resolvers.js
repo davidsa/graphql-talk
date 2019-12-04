00751b5defb60e7e6af0c26565fed03abd14d7b4
David Sttivend <dsangel@gmail.com>
2019-12-04 10:05:45 -0500

    Implement resolvers for adding and deleting pets

--- workshop/server/src/resolvers.js
+++ workshop/server/src/resolvers.js
@@ -1,3 +1,13 @@
-const resolvers = {};
+const PetModel = require('./models');
+
+const resolvers = {
+  Query: {
+    pets: () => PetModel.find(),
+  },
+  Mutation: {
+    addPet: (_, {petInput}) => PetModel.create(petInput),
+    deletePet: (_, {petId}) => PetModel.findOneAndDelete({_id: petId}),
+  },
+};
 
 module.exports = resolvers;
--- workshop/server/src/typeDefs.js
+++ workshop/server/src/typeDefs.js
@@ -1,5 +1,27 @@
 const {gql} = require('apollo-server');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Query {
+    pets: [Pet!]!
+  }
+
+  type Mutation {
+    addPet(petInput: PetInput!): Pet
+    deletePet(petId: ID): Pet
+  }
+
+  type Pet {
+    _id: ID
+    name: String!
+    age: Int!
+    race: String!
+  }
+
+  input PetInput {
+    name: String!
+    age: Int!
+    race: String!
+  }
+`;
 
 module.exports = typeDefs;
